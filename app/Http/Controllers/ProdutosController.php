396978d0255c1f19c9e7e4a371bbc95f69987104
jorge <jorgecalheiros.s@gmail.com>
2022-03-16 22:05:16 -0300

    ajuste no observe

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Produtos\StoreProdutos;
-use App\Http\Requests\Produtos\UpdateProdutos;
 use App\Repositories\Contracts\ProdutoRepositoryContract;
 use Exception;
 use Illuminate\Http\Request;
