a5d8a3bc035e9adf871bbd355cd3fa9dcb7c5a87
jorge <jorgecalheiros.s@gmail.com>
2022-03-21 17:26:09 -0300

    observer de registros

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Produtos;
+use App\Models\Registros;
 use App\Observers\Produtos\ProdutosObserver;
+use App\Observers\RegistroObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -30,5 +32,6 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         Produtos::observe(ProdutosObserver::class);
+        Registros::observe(RegistroObserver::class);
     }
 }
